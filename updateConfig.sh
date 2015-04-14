437cf7fc8d0f8476d94393a8a89048b2d36f014d
Leonard Truong <leonardtruong@gmail.com>
2015-04-13 17:43:53 -0700

    Add upd server to updateConfig

--- updateConfig.sh
+++ updateConfig.sh
@@ -2,6 +2,8 @@
 screen -S chairDriver -X quit
 screen -S portServer -X quit
 screen -S actuateServer -X quit
+screen -S udpReportingServer -X quit
 screen -d -m -S chairDriver twistd -n smap chair.ini
 screen -d -m -S portServer python portServer.py
 screen -d -m -S actuateServer python actuateServer.py
+screen -d -m -S udpReportingServer python udp_reporting_server.py
