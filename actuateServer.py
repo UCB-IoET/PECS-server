664a8cafc026f05d0e5b5acd5a63d41a1afb4a21
Sam Kumar <samkumar@berkeley.edu>
2015-05-07 00:09:52 +0000

    Dynamically infer IP Addresses of chairs server-side

--- actuateServer.py
+++ actuateServer.py
@@ -21,7 +21,7 @@ parser = ConfigParser.RawConfigParser()
 parser.read('chair.ini')
 for sect in parser.sections():
     if parser.has_option(sect, 'macaddr') and parser.has_option(sect, 'rel_ip') and parser.has_option(sect, 'dest_ip') and parser.has_option(sect, 'port'):
-        ipmap[parser.get(sect, 'macaddr')] = (parser.get(sect, 'rel_ip'), parser.get(sect, 'dest_ip'), parser.get(sect, 'port'))
+        ipmap[parser.get(sect, 'macaddr')] = [parser.get(sect, 'rel_ip'), parser.get(sect, 'dest_ip'), parser.get(sect, 'port')]
 
 FS_PORT = 60001
     
@@ -69,8 +69,8 @@ class ActuationHandler(BaseHTTPRequestHandler):
         print "Successfully updated sMAP"
         removeList = []
         timestamp = int(res.text)
-        if 'fromIP' in doc:
-            ips[0] = doc['fromIP']
+        if 'myIP' in doc:
+            ips[1] = doc['myIP']
         if 'fromFS' not in doc or not doc['fromFS']:
             for key in doc:
                 if key not in ["backh", "bottomh", "backf", "bottomf", "heaters", "fans"]:
